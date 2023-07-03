465430f7fb67fb55bb669504a363fa40e72adfa8
Paulo Felipe <paulofelipetj@hotmail.com>
2023-07-03 20:56:17 -0300

    Menu principal

---
+++ src/ConversorMoedas.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class ConversorMoedas {
+    String valor = JOptionPane.showInputDialog("Escolha uma valor");
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-       
+
+        String optionOne = "Conversor de moedas";
+        String optionTwo = "Conversor de temperatura";
+        String[] options = {optionOne, optionTwo};
+
+        String selectedOption = (String) JOptionPane.showInputDialog(null, "Escolha uma opção:", "Conversor",
+                JOptionPane.PLAIN_MESSAGE, null, options, optionOne);
+
+        if (selectedOption.equals(optionOne)){
+            ConversorMoedas conversorMoedas = new ConversorMoedas();
+        }
+
+
     }
 }
